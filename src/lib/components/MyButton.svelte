e3118b979fb15acf2996f5be84995796b40779a4
Daniel Lucchesi <dlucchesi@gmail.com>
2025-05-29 17:32:20 -0300

    Init

---
+++ src/lib/components/MyButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+  type Props = {
+    href?: string;
+    children?: Snippet;
+    label?: string;
+  }
+  let {label,children, href}: Props = $props()
+
+</script>
+
+<a href="{href}" class="bg-green-700 flex-1/2 background-gray-900 text-white px-3 py-2 rounded text-sm font-medium hover:bg-gray-700">
+  {label ? label : 'Click Me'}
+</a>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+	import MyButton from "$lib/components/MyButton.svelte";
+
+</script>
+<div class="container mx-auto">
+  <p class="mb-4">This is a simple Svelte application to track your weight and glucose levels.</p>
+  <p class="mb-4">Use the navigation bar to access different sections of the app.</p>
+<MyButton href="/" label="weight" />
+</div>
